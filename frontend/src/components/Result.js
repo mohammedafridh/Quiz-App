60dc225ad861c4234e80f09bb2493b357497ab43
Afridh <afridh7788@gmail.com>
2023-08-13 20:34:03 +0530

    Modify changes

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -7,15 +7,15 @@ const Result = ({ showResult, quizs, marks, startOver }) => {
         const total = (marks / 50) * 100
 
         if(total<50){
-            return <h2>Your are a digital marketing novice.</h2>
+            return <h2 style = {{color:'red'}}>Your are a digital marketing novice.</h2>
         }else if(total>=50 && total<=60){
-            return <h2>Your are a digital marketing seed.</h2>
+            return <h2 style = {{color:'#229954'}}>Your are a digital marketing seed.</h2>
         }else if(total>=61 && total<=70){
-            return <h2>Your are a digital marketing rising star.</h2>
+            return <h2 style = {{color:'#148F77'}}>Your are a digital marketing rising star.</h2>
         }else if(total>=71 && total<=80){
-            return <h2>Your are a digital marketing star.</h2>
+            return <h2 style = {{color:'#1F618D'}}>Your are a digital marketing star.</h2>
         }else{
-            return <h2>Your are a digital marketing rock star.</h2>
+            return <h2 style = {{color:'#884EA0'}}>Your are a digital marketing rock star.</h2>
         }
     }
 
@@ -31,7 +31,7 @@ const Result = ({ showResult, quizs, marks, startOver }) => {
                             <h1 className='resultStatus'>{calculateScore()}</h1>
                             <h3 className={`resultText ${marks > 24 ? 'high' : 'low'}`}>Your score is {marks} out of {quizs.length * 5}</h3>
 
-                            <button type = 'submit' className='btn startOverBtn'>Start Over</button>
+                            <button type = 'submit' className='btn startOverBtn'>START AGAIN</button>
                         </div>
                     </div>
                 </div>
